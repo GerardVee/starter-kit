211e3447769032ba133f4c9ddf0d3a31e2c21a50
Gerardo Valenzuela <ilovesciencefb@gmail.com>
2017-11-10 14:29:22 -0800

    fixes

--- src/organisms/Body.js
+++ src/organisms/Body.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import styled from 'styled-components';
-import { SketchPicker } from 'react-color';
 
-import Button from '../atoms/Button';
 import Header from '../molecules/Header';
 import ButtonGroup from '../molecules/ButtonGroup';
 import CommentGroup from '../molecules/CommentGroup';
@@ -54,18 +52,6 @@ additionally, put down some of your projects in your resume, and describe what y
 
 class Body extends React.Component
 {
-    state =
-    {
-        shade: '#fff',
-        fill: '#fff',
-        stroke: '#fff'
-    };
-
-    handleChangeComplete(type, color)
-    {
-        this.setState({ [type]: color.hex });
-    }
-
     componentWillMount()
     {
         this.props.getBody();
@@ -83,16 +69,6 @@ class Body extends React.Component
         }
         return (
             <div className={ `container ${ className }` }>
-                {/*<SketchPicker color={ this.state.fill } onChangeComplete={ (color) => this.handleChangeComplete('fill', color) }/>
-                <SketchPicker color={ this.state.shade } onChangeComplete={ (color) => this.handleChangeComplete('shade', color) }/>
-                <SketchPicker color={ this.state.stroke } onChangeComplete={ (color) => this.handleChangeComplete('stroke', color) }/>
-                <Button bold fill={ this.state.fill } shade={ this.state.shade } stroke={ this.state.stroke }>Example</Button>*/}
-                <Button bold fill='#FADA5E' shade='white' stroke='none'>Add</Button>
-                <Button bold fill='none' shade='#FADA5E' stroke='#FADA5E'>Edit</Button>
-                <Button bold fill='none' shade='#FADA5E' stroke='#FADA5E'>Swap</Button>
-                <Button bold fill='none' shade='#9B30FF' stroke='#9B30FF'>Clear</Button>
-                <Button bold fill='none' shade='#9B30FF' stroke='#9B30FF'>Remove</Button>
-                <Button bold fill='#9B30FF' shade='white' stroke='none'>Delete</Button>
                 <Header contents={ contents }/>
                 <ButtonGroup comments={ comments } toggleComment={ (id) => this.props.toggleComment(id) }/>
                 <CommentGroup comments={ comments }/>
